e836ba5710e6581f381f96d6c158c16b4d52e04c
1013180 <hiroki.kojima0417@gmail.com>
2021-01-13 12:24:44 +0000

    [Start]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5e310578866e56820c5544e5eedacf4304e84ac583c5e9c34dc2c9b60bf3326475b544ec1c31d494b813c837d5bf63970aeb94e62c62b53c0ccc0005ec9b36a1'
